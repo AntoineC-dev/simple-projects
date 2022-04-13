c6fa5602d8c7b3c432e85e8b4228856928c49a4f
Antoine Cheminat <ac.dev@hotmail.com>
2022-04-13 01:32:14 -0400

    Update styles

--- simple-calculator/src/App.tsx
+++ simple-calculator/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { DigitButton, OperationButton } from "./components";
 import { useClear, useOutput, useRemoveDigit, useSetResult } from "./controllers";
 import { formatOperand } from "./helpers";
+import arrowBackWhite from "./assets/arrow_back_white.png";
 
 function App() {
   const { currentOperand, operation, previousOperand } = useOutput();
@@ -16,30 +17,32 @@ function App() {
         </div>
         <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
-      <button type="button" className="span-two" onClick={clear}>
-        AC
-      </button>
-      <button type="button" onClick={removeDigit}>
-        DEL
-      </button>
-      <OperationButton operation="÷" />
-      <DigitButton digit="1" />
-      <DigitButton digit="2" />
-      <DigitButton digit="3" />
-      <OperationButton operation="x" />
-      <DigitButton digit="4" />
-      <DigitButton digit="5" />
-      <DigitButton digit="6" />
-      <OperationButton operation="-" />
-      <DigitButton digit="7" />
-      <DigitButton digit="8" />
-      <DigitButton digit="9" />
-      <OperationButton operation="+" />
-      <DigitButton digit="." />
-      <DigitButton digit="0" />
-      <button type="button" className="span-two" onClick={setResult}>
-        =
-      </button>
+      <div className="calculator__actions">
+        <button type="button" className="span-two" onClick={clear}>
+          AC
+        </button>
+        <button type="button" onClick={removeDigit}>
+          <img src={arrowBackWhite} alt="" style={{ width: "1rem" }} />
+        </button>
+        <OperationButton operation="÷" />
+        <DigitButton digit="1" />
+        <DigitButton digit="2" />
+        <DigitButton digit="3" />
+        <OperationButton operation="x" />
+        <DigitButton digit="4" />
+        <DigitButton digit="5" />
+        <DigitButton digit="6" />
+        <OperationButton operation="-" />
+        <DigitButton digit="7" />
+        <DigitButton digit="8" />
+        <DigitButton digit="9" />
+        <OperationButton operation="+" />
+        <DigitButton digit="." />
+        <DigitButton digit="0" />
+        <button type="button" className="span-two" onClick={setResult}>
+          =
+        </button>
+      </div>
     </div>
   );
 }
